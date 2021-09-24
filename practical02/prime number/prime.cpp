82430f4bd562b6d3e6691e74ad74da9751bcb808
AshutoshRawat007 <91238320+AshutoshRawat007@users.noreply.github.com>
2021-09-24 17:50:31 +0530

    Add files via upload
    Program to check for prime number without using for loop.
    Condition is checked if number is divisible by 2,3,5,7 or its own square root   ,
     if yes its not a prime number else its a prime.
    
    Simple yet efficient way to check for prime.

---
+++ practical02/prime number/prime.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+int squareroot(int n);
+
+int prime(int n)
+{
+int c=0;
+if(n%2==0&&n!=2)
+c++;
+if(n%3==0&&n!=3)
+c++;
+if(n%3==0&&n!=5)
+c++;
+if(n%3==0&&n!=7)
+c++;
+if(n%squareroot(n)==0)
+c++;
+
+if(c==0)
+return 1;
+if(c!=0)
+return 0;
+}
+
+int squareroot(int n)
+{
+if (n == 0 || n== 1)
+    return n;
+ int i = 1, result = 1;
+    while (result <= n)
+    {
+      i++;
+      result = i * i;
+    }
+    return i - 1;
+}
+
+int main()
+{
+int n;
+cout<<"enter the number"<<endl;
+cin>>n;
+if(prime(n)==1)
+cout<<"the number is prime"<<endl;
+else
+cout<<"number is not prime"<<endl;
+
+}
+
