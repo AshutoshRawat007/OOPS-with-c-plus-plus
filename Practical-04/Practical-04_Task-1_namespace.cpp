14a41b2bce0908e9f0cf0be6949eac3e81631737
AshutoshRawat007 <91238320+AshutoshRawat007@users.noreply.github.com>
2021-09-29 10:20:21 +0530

    Update Practical-04_Task-1_namespace.cpp

--- Practical-04/Practical-04_Task-1_namespace.cpp
+++ Practical-04/Practical-04_Task-1_namespace.cpp
@@ -1,12 +1,12 @@
 #include<iostream>
 namespace first{
   void add(int n1,int n2){
-    cout<<"sum of integers = "<<n1+n2<<endl;
+   std:: cout<<"sum of integers = "<<n1+n2;
   }
 }
 namespace second{
   void add(float n1,float n2){
-    cout<<"sum of floats = "<<n1+n2<<endl;
+    std::cout<<"sum of floats = "<<n1+n2;
   }
 }
 int main()
