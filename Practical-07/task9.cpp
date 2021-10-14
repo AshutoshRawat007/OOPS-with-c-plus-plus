3bd05ce2674b047057ff86cca1d29ea486a44e09
AshutoshRawat007 <91238320+AshutoshRawat007@users.noreply.github.com>
2021-10-14 10:17:46 +0530

    Add files via upload

---
+++ Practical-07/task9.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n=4;
+	cout<<"enter the elements from 1-16 sequencially";
+	for(int i=0;i<n;i++)
+		for(int j=0;j<n;j++)
+			cin>>a[i][j];
+	
+			
+			int x=1,i=0,i=0,top=0,bot=3;
+		while(x<=16)
+		{
+			if(i<=top&&j<=bot)
+			{ cout<<a[i][j]<<" "; j++; break;}
+
+			if(i<=bot
+		
+		
+		}
+				
+	}
+}
