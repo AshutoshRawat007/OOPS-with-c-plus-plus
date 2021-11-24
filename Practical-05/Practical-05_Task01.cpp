8f4da023500cbf5f040e942c460f9372649da54e
AshutoshRawat007 <91238320+AshutoshRawat007@users.noreply.github.com>
2021-11-24 21:33:29 +0530

    Add files via upload

---
+++ Practical-05/Practical-05_Task01.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+	char ch='a';
+	bool b= true;
+	short st=12;
+	int in =69;
+	long l=123456789;
+	float fl=12.12;
+	doub;e db =256.256;
+	long double ldb=5689.2356;
+	wchar_t wch =L'大';
+	cout<<"char -> " <<ch <<endl;
+	cout<<"bool -> " << <<endl;
+	cout<<"short -> " << st <<endl;
+	cout<<"int -> " <<in <<endl;
+	cout<<"long -> " <<l <<endl;
+	cout<<"flaot -> " <<fl <<endl;
+	cout<<"double -> " << db<<endl;
+	cout<<"long double -> " << ldb<<endl;
+	cout<<"wide char -> " << wch<<endl;
+}
---
+++ Practical-05/Practical-05_Task02.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+	char ch='a';
+	bool b= true;
+	short st=12;
+	int in =69;
+	long l=123456789;
+	float fl=12.12;
+	doub;e db =256.256;
+	long double ldb=5689.2356;
+	wchar_t wch =;
+	cout<<"char -> " <<sizeof(ch) <<endl;
+	cout<<"bool -> " << sizeof(b)<<endl;
+	cout<<"short -> " << sizeof(st) <<endl;
+	cout<<"int -> " <<sizeof(in) <<endl;
+	cout<<"long -> " <<sizeof(l) <<endl;
+	cout<<"flaot -> " <<sizeof(fl) <<endl;
+	cout<<"double -> " << sizeof(db)<<endl;
+	cout<<"long double -> " << sizeof(ldb) <<endl;
+	cout<<"wide char -> " << sizeof(wch) <<endl;
+}
